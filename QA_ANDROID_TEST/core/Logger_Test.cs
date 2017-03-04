fe4d8b29799b940ea617831b3a2cf2a67443844d
Marcos Barrera <Marcos Barrera>
2017-03-04 17:01:58 -0600

    push inicial

---
+++ QA_ANDROID_TEST/core/Logger_Test.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QA_ANDROID_TEST.core
+{
+    public class Logger_Test
+    {
+        public string LOG_NAME { get; set; }
+    }
+}
